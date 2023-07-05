415c457eed784b4b1f5d088e21ef96e527d83ba7
Erick Dourado <erickdourado_@hotmail.com>
2023-07-04 21:28:36 -0300

    Create user controller interface

---
+++ src/main/java/com/erick/webfluxcourse/controller/UserController.java
@@ -0,0 +1,26 @@
+package com.erick.webfluxcourse.controller;
+
+import com.erick.webfluxcourse.entity.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public interface UserController {
+
+    @PostMapping
+    ResponseEntity<Mono<UserResponse>> save(@RequestBody UserRequest userRequest);
+
+    @GetMapping("/{id}")
+    ResponseEntity<Mono<UserResponse>> findById(@PathVariable String id);
+
+    @GetMapping
+    ResponseEntity<Flux<UserResponse>> findAll();
+
+    @PatchMapping("/{id}")
+    ResponseEntity<Mono<UserResponse>> update(@PathVariable String id, @RequestBody UserRequest userRequest);
+
+    @DeleteMapping("/{id}")
+    ResponseEntity<Mono<Void>> delete(@PathVariable String id);
+
+}
