b2d93f802aae16f35dac301d02e33dbec21068d8
Erick Dourado <erickdourado_@hotmail.com>
2023-07-06 22:13:19 -0300

    Create exception handler, to validation error

--- src/main/java/com/erick/webfluxcourse/exception/ControllerExceptionHandler.java
+++ src/main/java/com/erick/webfluxcourse/exception/ControllerExceptionHandler.java
@@ -3,8 +3,10 @@ package com.erick.webfluxcourse.exception;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.support.WebExchangeBindException;
 import reactor.core.publisher.Mono;
 
 import static java.time.LocalDateTime.now;
@@ -26,6 +28,21 @@ public class ControllerExceptionHandler {
                                 .message(verifyDuplicateKey(e.getMessage()))
                                 .build()
                 ));
+    }
+
+    @ExceptionHandler(WebExchangeBindException.class)
+    public ResponseEntity<Mono<ValidationError>> validationError(WebExchangeBindException e,
+                                                                 ServerHttpRequest request) {
+        ValidationError error = new ValidationError(
+                now(), request.getPath().toString(),
+                BAD_REQUEST.value(), "Validation error", "Error on validation attributes"
+        );
+
+        for (FieldError fieldError : e.getBindingResult().getFieldErrors()) {
+            error.addError(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+
+        return ResponseEntity.badRequest().body(Mono.just(error));
     }
 
     private String verifyDuplicateKey(String message) {
